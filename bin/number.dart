4ddd712581da0487d0ebd4f0e21ea5569ebd7969
alwinurnadzif <alwinurnadzif.dev@gmail.com>
2024-07-11 15:21:20 +0700

    number

---
+++ bin/number.dart
@@ -0,0 +1,14 @@
+void main() {
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print('number1: $number1');
+  print('number2: $number2');
+
+  // num: dynamic can be change to int or double
+
+  num dynamicNumber = 10;
+  print('dynamicNumber: $dynamicNumber');
+  dynamicNumber = 11.4;
+  print('dynamicNumber: $dynamicNumber');
+}
