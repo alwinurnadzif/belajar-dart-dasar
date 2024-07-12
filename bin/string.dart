579160735d7165092a087515fbedf90b5c8dcc2b
alwinurnadzif <alwinurnadzif.dev@gmail.com>
2024-07-12 17:56:19 +0700

    string and dynamic

---
+++ bin/string.dart
@@ -0,0 +1,15 @@
+void main() {
+  print('This is string');
+
+  String firstName = "Mamat";
+  String lastName = "Gun Shop";
+
+  print('fullname: $firstName $lastName');
+
+  String longText = '''
+  Hello this 
+  looong string
+  ''';
+
+  print(longText);
+}
